56abe8a07e85e77163ac09e4dd4708756a7617b5
unknown <1017370718@qq.com>
2016-09-26 23:10:27 +0800

    添加即将上映模块

--- app/coming_soon/controller.js
+++ app/coming_soon/controller.js
@@ -4,7 +4,7 @@
 	var module = angular.module('moviecat.coming_soon', ['ngRoute']);
 
 		module.config(['$routeProvider', function($routeProvider) {
-			$routeProvider.when('/coming_soon', {
+			$routeProvider.when('/coming_soon/:page', {
 				templateUrl: 'coming_soon/view.html',
 				controller: 'ComingSoonController'
 			});
@@ -12,8 +12,32 @@
 
 		module.controller('ComingSoonController', [
 				'$scope',
-				function($scope) {
-
+				'$route',
+				'$routeParams',
+				'HttpService',
+				function($scope,$route,$routeParams,HttpService) {
+					var count = 3;
+					var currentPage = parseInt($routeParams.page);
+					var start = (currentPage-1)*count;
+					$scope.loading = false;
+				HttpService.jsonp('http://api.douban.com/v2/movie/coming_soon',
+					{
+						start:start,
+						count:count
+					},function(data){
+						$scope.title = data.title;
+						$scope.subjects=data.subjects;
+						$scope.currentPage = currentPage;
+						$scope.totalCount = data.total;
+						$scope.totalPage = Math.ceil($scope.totalCount/count);
+						console.log(data)
+						$scope.loading = true;
+						$scope.$apply();
+					});
+					$scope.go = function(page){
+						if (page >=1 && page<=$scope.totalPage)
+							$route.updateParams({ page:page });
+					}
 		}]);
 })(angular)
 
