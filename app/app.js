7ddb16a220ea2b0e394ab19842fde7745d53ed06
unknown <1017370718@qq.com>
2016-09-28 00:17:18 +0800

    add tab directive

--- app/app.js
+++ app/app.js
@@ -4,7 +4,8 @@
 angular.module('movicecat', [
   'ngRoute',
 	'moviecat.services.http',
-  	'moviecat.category'
+  	'moviecat.category',
+	'moviecat.directive.auto_focus'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/in_theaters/1'});
---
+++ app/components/auto-focus.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Administrator on 2016/9/27.
+ */
+(function(angular){
+	angular.module('moviecat.directive.auto_focus',[])
+		.directive('autoFocus',['$location',function($location){
+			return{
+				restrict: 'A',
+				link: function($scope, iElm, iAttrs, controller){
+					$scope.$location = $location;
+					$scope.$watch('$location.path()', function(now) {
+						var aLink = iElm.children().attr('href');
+						var type = aLink.replace(/#(\/.+?)\/\d+/, '$1');
+						if(now.startsWith(type)){
+							iElm.parent().children().removeClass('active');
+							iElm.addClass('active');
+						}
+					})
+				}
+			}
+		}])
+})(angular)
